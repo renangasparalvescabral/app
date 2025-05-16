73659b4cd067ab6e7e5ac82d5f8ef9809a10ed44
Renan Gaspar <renangasparalvescabral@gmail.com>
2025-05-15 22:40:21 -0300

    Organização das pastas

---
+++ vendor/FrameworkAULA/System.php
@@ -0,0 +1,2 @@
+namespace frameworkAULA;
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'son/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6dc34adb29a623a1cf40ae1e0e2cd7064afb0a24',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'son/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6dc34adb29a623a1cf40ae1e0e2cd7064afb0a24',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
